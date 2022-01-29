1bd97948beb0f3e163a1e2d3fa7a717a777ab19c
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-29 23:17:39 +0900

    Add : Landing Page 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,21 @@
-import React, {useState} from "react";
-import {QUIZZES} from "../constants";
-import {ThemeProvider} from "styled-components";
+import React from "react";
+import { ThemeProvider } from "styled-components";
 import theme from "../theme";
-import Container from "./Container"
-import AnswerGroup from "./AnswerGroup";
-import QuestionSection from "./QuestionSection";
-import ResultSection from "./ResultSection";
 import GlobalStyle from "../globalStyle";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Quiz from "../pages/Quiz";
+import Landing from "../pages/Landing";
 
 function App() {
-  const [currentNo, setCurrentNo] = useState(0);
-  const [showResult, setShowResult] = useState(false);
-  const [score, setScore] = useState(0);
-
-  const handleClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore((score) => score + 1);
-		}
-    if(currentNo === QUIZZES.length-1) {
-      setShowResult(true);
-    } else {
-      setCurrentNo((currentNo) => currentNo + 1);
-    }
-  };
-  const convertedScore = Math.floor((score / QUIZZES.length)*100);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-      {showResult ? (
-        <Container>
-          <ResultSection convertedScore={convertedScore}></ResultSection>
-        </Container>
-      ) : (
-        <Container>
-          <QuestionSection currentNo={currentNo}/>
-          <AnswerGroup currentNo={currentNo} handleClick={handleClick}/>
-        </Container>
-      )}
-  </ThemeProvider>    
-);
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Router>
+            <Route path="/quiz" component={Quiz} />
+            <Route path="/" exact component={Landing} />
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/pages/Landing/index.js
@@ -0,0 +1,51 @@
+import Container from "../../components/Container";
+import styled from "styled-components";
+import image from "../../components/assets/images/img.jpg";
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
+
+const Title = styled.h1`
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 8px;
+    text-align: center;
+    color: ${(props) => props.theme.primaryColor100};
+`;
+
+const SubTitle = styled.h1`
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 24px;
+    text-align: center;
+    color: ${(props) => props.theme.gray80};
+`;
+
+const StyledImage = styled.img`
+    border-radius: 16px;
+    opacity: 80%;
+    margin-bottom: 16px;
+    max-width: 100%;
+    display: block;
+`;
+
+const Text = styled.p`
+    font-size: 14px;
+    margin-bottom: 24px;
+    color: ${(props) => props.theme.gray80};
+`;
+
+const Landing = () => (
+    <Container>
+        <Title>무라카미 하루키 애정도 테스트</Title>
+        <SubTitle>당신은 하루키에 대해 얼마나 알고 있나요?</SubTitle>
+        <StyledImage src={image} alt="image"></StyledImage>
+        <Text>
+            무라카미 하루키는 한국 독자의 꾸준한 사랑을 받고 있습니다. 당신도 하루키를 좋아하시나요?
+        </Text>
+        <Link to="/quiz">
+            <Button text="내가 얼마나 하루키를 좋아하는지 알아보기"></Button>
+        </Link>
+    </Container>
+);
+
+export default Landing;
